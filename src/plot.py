ecf9c74bd9eba042782156a77632ae1f788da43e
alexsanjoseph <alexsanjoseph@gmail.com>
2020-11-01 11:27:26 +0530

    Moved venv separately

--- src/plot.py
+++ src/plot.py
@@ -3,7 +3,7 @@ from pandas import to_datetime
 
 def plot_and_save(scale_data_df_cleaned, smooth_factor, temp_file_name):
     fasting_start = to_datetime('2019-10-15')
-    plot_output = (ggplot(scale_data_df_cleaned, aes(x = 'timestamp', y = 'weight')) + 
+    plot_output = (ggplot(scale_data_df_cleaned, aes(x = 'timestamp', y = 'weight')) +
         #   facet_wrap('~', ncol = 1, scales = 'free') +
         geom_point(size = 0.5) +
         geom_smooth(span = smooth_factor, color = 'red') +
