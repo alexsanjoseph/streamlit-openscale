c56748e44e3ab82d4ff3ef109ebda976298ff6d3
alexsanjoseph <alexsanjoseph@gmail.com>
2020-11-01 15:43:27 +0530

    Removed cruft

--- src/dropbox_aux.py
+++ src/dropbox_aux.py
@@ -1,11 +1,6 @@
 import pandas as pd
 from io import StringIO
 
-
-def get_dropbox_key():
-    pass
-
-
 def get_latest_file_name_from_dropbox(dbx, folder_name, username):
     files_list = dbx.files_search(folder_name, query=username + "*.csv")
     return sorted([(x.metadata.name, str(x.metadata.server_modified))  for x in files_list.matches], key = lambda x: x[1])[-1][0]
